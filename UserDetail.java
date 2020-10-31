ceb18d2394c004eabb1325f1dd49d0e2b78ffdc6
Pawan Kumar <pk.soft29@gmail.com>
2020-10-31 10:24:57 +0530

    Refactor : To validate all sample Emails

--- UserDetail.java
+++ UserDetail.java
@@ -2,13 +2,30 @@ public class UserDetail {
 
     //Main method
     public static void main(String[] args) {
-        UserDetail userDetail = new UserDetail();
-        System.out.println(userDetail.checkFName("Pawan"));
-        System.out.println(userDetail.checkLName("Kumar"));
-        System.out.println(userDetail.checkEmail("pk.soft29@gmail.com"));
-        System.out.println(userDetail.checkMobileNumber("91 8083373213"));
-        System.out.println(userDetail.checkPassword("sgsP@sghfsgt1"));
+        UserDetail userDetail =  new UserDetail();
+        //passing valid email Id
+        System.out.println("All Are valid email Id");
+        System.out.println(userDetail.checkEmail("abc@yahoo.com"));
+        System.out.println(userDetail.checkEmail("abc-100@yahoo.com"));
+        System.out.println(userDetail.checkEmail("abc.100@yahoo.com"));
+        System.out.println(userDetail.checkEmail("abc111@abc.com"));
+        System.out.println(userDetail.checkEmail("abc-100@abc.net"));
+        System.out.println(userDetail.checkEmail("abc.100@abc.com.au"));
+        System.out.println(userDetail.checkEmail("abc@1.com"));
+        System.out.println(userDetail.checkEmail("abc@gmail.com.com"));
+        System.out.println(userDetail.checkEmail("abc+100@gmail.com"));
 
+        //passing Invalid email Id
+        System.out.println("All Are Invalid email Id");
+        System.out.println(userDetail.checkEmail("abc"));
+        System.out.println(userDetail.checkEmail("abc@yahoo.my"));
+        System.out.println(userDetail.checkEmail("abc.123@gmail.1"));
+        System.out.println(userDetail.checkEmail("abc123@.com"));
+        System.out.println(userDetail.checkEmail("abc123@.com.com"));
+        System.out.println(userDetail.checkEmail(".abc@abc.com.au"));
+        System.out.println(userDetail.checkEmail("abc()*@gmail.com"));
+        System.out.println(userDetail.checkEmail("abc@%*.com"));
+        System.out.println(userDetail.checkEmail("abc..2002@gmail.com"));
     }
 
     //  This method is used to validate the First Name
