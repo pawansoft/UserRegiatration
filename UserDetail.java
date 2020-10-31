2be0ee84d040853d288d3e8868f362c5d39cf662
Pawan Kumar <pk.soft29@gmail.com>
2020-10-31 09:44:39 +0530

    Added : function to check validate number*.java

--- UserDetail.java
+++ UserDetail.java
@@ -6,6 +6,7 @@ public class UserDetail {
         System.out.println(userDetail.checkFName("Pawan"));
         System.out.println(userDetail.checkLName("Kumar"));
         System.out.println(userDetail.checkEmail("pk.soft29@gmail.com"));
+        System.out.println(userDetail.checkMobileNumber("91 8083373213"));
 
     }
 
@@ -20,8 +21,15 @@ public class UserDetail {
     }
 
     //  This method is used to validate email Id
-    public boolean checkEmail(String email){
+    public boolean checkEmail(String email) {
         return (email.matches("^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+([.][a-zA-Z]{2,4})*$"));
     }
+
+    //This method is used to validate Mobile Number
+    public boolean checkMobileNumber(String mobileNumber){
+        return (mobileNumber.matches("^[0-9]{1,2}[ ][0-9]{10}$"));
+    }
 }
+
+
 
